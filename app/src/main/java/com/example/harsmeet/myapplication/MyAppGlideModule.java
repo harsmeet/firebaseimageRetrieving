b44149ec81f46d7367160971d2afcdae8e3b467c
harsmeet <er.harsmeet@gmail.com>
2019-01-28 22:30:01 +0530

    AppGlide module continue....

--- app/src/main/java/com/example/harsmeet/myapplication/MyAppGlideModule.java
+++ app/src/main/java/com/example/harsmeet/myapplication/MyAppGlideModule.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.Registry;
+
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.module.AppGlideModule;
 import com.firebase.ui.storage.images.FirebaseImageLoader;
@@ -19,10 +20,14 @@ public class MyAppGlideModule extends AppGlideModule {
     @Override
     public void registerComponents(Context context, Glide glide, Registry registry) {
         // Register FirebaseImageLoader to handle StorageReference
+
+
         registry.append(StorageReference.class, InputStream.class,
+
+
                 new FirebaseImageLoader.Factory());
 
-        
+
     }
 }
 
